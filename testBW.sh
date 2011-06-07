97bbf9d6625eaf988bcea19739c2ae9658a59e87
Boris Klymko <boris.klymko@gmail.com>
2011-06-08 00:18:35 +0200

    Optim and fix (see details)
    Fix for my xen plateform (confusion between eth0 and peth0)
    wgets in a for loop

--- testBW.sh
+++ testBW.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 snap() {
-  sed -n 's/eth0: *\([0-9]\+\) .*/\1/p' /proc/net/dev
+  sed -n 's/ eth0: *\([0-9]\+\) .*/\1/p' /proc/net/dev
 }
 
 calculKBRate() {
@@ -8,23 +8,10 @@ calculKBRate() {
   echo $(date '+%Y%m%d-%H%M%S') $((($(snap) - $snapBegin) / 1024 / ($(date +%s) - $debutTime)))
 }
 
-wget -o /dev/null -O /dev/null http://debian.advalem.net/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://debian.ens-cachan.fr/ftp/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://debian.med.univ-tours.fr/debian-cd/current/amd64/iso-dvd/debian-503-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://debian.mines.inpl-nancy.fr/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://debian.mirror.inra.fr/debian-cd/current/amd64/iso-dvd/debian-503-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://mirror.cict.fr/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://mirror.linuxmint-fr.net/linuxmint.com/stable/9/linuxmint-9-kde-dvd-i386.iso &
-wget -o /dev/null -O /dev/null http://ftp.cica.es/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp.gva.es/mirror/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp.udc.es/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp5.gwdg.de/pub/linux/debian/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp.de.debian.org/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp.halifax.rwth-aachen.de/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://vesta.informatik.rwth-aachen.de/ftp/pub/comp/Linux/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp.informatik.uni-frankfurt.de/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp-stud.hs-esslingen.de/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
-wget -o /dev/null -O /dev/null http://ftp.uni-bayreuth.de/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso &
+for serveur in debian.advalem.net debian.ens-cachan.fr debian.mines.inpl-nancy.fr mirror.cict.fr ftp.cica.es ftp.udc.es ftp.de.debian.org ftp.halifax.rwth-aachen.de ftp.informatik.uni-frankfurt.de ftp-stud.hs-esslingen.de ftp.uni-bayreuth.de; do
+  wget -o /dev/null -O /dev/null 'http://'$serveur'/debian-cd/current/amd64/iso-dvd/debian-6.0.1a-amd64-DVD-1.iso' &
+done
+
 sleep 20
 #jobs
 snapBegin=$(snap)
@@ -37,6 +24,6 @@ debutTime=$(date +%s)
   $(calculKBRate 30)
   $(calculKBRate 30)
 }
-for i in {1..17}; do kill %$i; done
+for i in {1..11}; do kill %$i; done
 #sleep 5; jobs
 
